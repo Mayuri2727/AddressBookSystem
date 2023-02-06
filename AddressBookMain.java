161685630985a03dcbfda0fb2f0cda68fc8bf75a
Mayuri2727 <mayuriasabe107@gmail.com>
2023-02-06 20:25:40 +0530

    Creat Contacts

---
+++ AddressBookMain.java
@@ -0,0 +1,8 @@
+package AddressBookSystem;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+            AddressBook addressBook = new AddressBook();
+            addressBook.addContact();
+       }
+    }
